1605e548dd2da347ed6960e148a12022276afbeb
david <davidwang@lucasgc.com>
2020-04-07 18:00:14 +0800

    yaml

--- utils/yaml/yaml.go
+++ utils/yaml/yaml.go
@@ -8,42 +8,6 @@ import (
         "gopkg.in/yaml.v2"
 )
 
-// Note: struct fields must be public in order for unmarshal to
-// correctly populate the data.
-type App struct {
-    Db struct {
-        Default Db
-    }
-    Redis struct {
-        Default Redis
-    }
-    OAuth2 struct {
-        Client []Client `yaml:"client"`
-    } `yaml:"oauth2"`
-}
-
-type Db struct {
-    Type string `yaml:"type"`
-    Host string `yaml:"host"`
-    Port int `yaml:"port"`
-    User string `yaml:"user"`
-    Password string `yaml:"password"`
-    DbName string `yaml:"dbname"`
-}
-
-type Redis struct {
-    Addr string `yaml:"addr"`
-    Password string `yaml:"password"`
-    Db int `yaml:"db"`
-}
-
-type Client struct {
-    ID string `yaml:"id"`
-    Secret string `yaml:"secret"`
-    Name string `yaml:"name"`
-    Domain string `yaml:"domain"`
-}
-
 var Cfg App
 
 func Setup() {
